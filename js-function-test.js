f4cb294b63b48a7691bc4208ca0850d366727f94
I-the-Creator <andrey.burluckiy@gmail.com>
2022-06-18 11:07:16 +0200

    no bs ts part 2-3

---
+++ js-function-test.js
@@ -0,0 +1,25 @@
+const { getName, getNameTwo } = require("./functions");
+
+console.log(
+    getName({
+        firstName: "a",
+        lastName: "b"
+    })
+);
+
+console.log(getNameTwo());   // first last - set by default
+console.log(getNameTwo({firstName: "bb", lastName: "fff"})); // bb fff
+
+
+
+
+// CLOSURE
+function createAdder(num) {      
+    return (val) => num + val
+}
+
+const creator = createAdder(5)
+console.log(creator(10));   // 15
+
+const creator2 = createAdder(20)
+console.log(creator2(50))   // 70
